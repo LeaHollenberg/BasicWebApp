e4459d389c0ddb92858dd8981e8e078d18e848ae
Lea Hollenberg <Lea.Hollenberg@campu.lmu.de>
2022-07-14 17:37:27 +0200

    Name

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,8 +13,19 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "Lea";
-        } else { // TODO extend the programm here
-            return "";
+        } else if (query.contains("which of the following numbers is the largest:"){ // TODO extend the programm here
+            String[] tmp = query.split(": ");
+            String[] numbers = query.split(", ");
+
+            int[] arr2 = new int[numbers.length];
+            for (int i = 0; i < numbers.length; i++) {
+                arr2[i] = Integer.parseInt(numbers[i]);
+            }
+
+            IntStream intStream = Arrays.stream(arr2);
+            OptionalInt optionalInt = intStream.max();
+            int maxAsInt = optionalInt.getAsInt();
+            return  maxAsInt;
         }
     }
 }
